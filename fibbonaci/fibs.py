f8eff56ab359a5ab3c17af7e69caccfbac550512
lucasjotap <leiathemosthot@gmail.com>
2024-02-13 09:39:51 -0300

    fibs

--- fibbonaci/fibs.py
+++ fibbonaci/fibs.py
@@ -1,7 +1,10 @@
+from typing import Dict
+
 class Fibbonaci(object):
 	"""
 	Class implements many variations of writing the fibbonaci algorithm.
 	"""
+	memo: Dict[int, int] = {0: 0, 1: 1} 
 
 	def fib_recursive(self, n: int) -> int:
 		"""
@@ -25,8 +28,14 @@ class Fibbonaci(object):
 			next = last + next 
 		return next
 
+	def fib_memoed(self, n: int) -> int:
+		if n not in self.memo:
+			self.memo[n] = self.fib_memoed(n-1) + self.fib_memoed(n-2)
+		return self.memo[n]
+
 if __name__ == "__main__":
 	f = Fibbonaci()
 	# fib_list = [f.fib_recursive(n) for n in range(0, 11)]
 	# print(fib_list) 
-	# print(f.fib_iterative(50))
+	# print(f.fib_iterative(50)
+	print(f.fib_memoed(800))
