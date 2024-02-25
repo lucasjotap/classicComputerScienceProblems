4af499fe1ff791f46824d5a966e541fa6071b41c
lucasjotap <leiathemosthot@gmail.com>
2024-02-24 23:45:18 -0300

    encryption

--- encryption/unbreakable_encryption.py
+++ encryption/unbreakable_encryption.py
@@ -7,9 +7,9 @@ class RandomKeyGenerator:
 		self.token_bytes_key = self.random_key(length)
 
 	def random_key(self, length: int) -> int:
-		return int.from_bytes(token_bytes(length), "big")
+		return int.from_bytes(token_bytes(length), "big")	
 
 if __name__ == '__main__':
 	rkg = RandomKeyGenerator(10)
 	rkg_key = rkg.token_bytes_key
-	print(rkg_key)
+	print(type (rkg_key))
